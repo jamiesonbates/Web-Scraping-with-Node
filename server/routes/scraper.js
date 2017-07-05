ec75cfcf95bdf8f46760bf392be94adb9ff0f341
Jamieson Bates <jamiesonbates@gmail.com>
2017-07-05 16:20:45 -0700

    Add in route

--- server/routes/scraper.js
+++ server/routes/scraper.js
@@ -5,7 +5,22 @@ const scrapeSeattleTimes = require('../bots/seattleTimes');
 const { insertStories } = require('../db/actions');
 
 router.get('/seattletimes', (req, res, next) => {
-  // use bots and actions to get data and persist
+  const url = 'http://www.seattletimes.com/';
+
+  scrapeSeattleTimes(url)
+    .then((data) => {
+      const { primary, secondary } = data;
+
+      primary.was_primary = true;
+
+      return Promise.all([insertStories([primary]), insertStories(secondary)]);
+    })
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
